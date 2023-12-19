5f8dbf8d6a34834196c2e92f587c9878a4d20146
Rushibhawar <rushikeshbhawar1213@gmail.com>
2023-12-19 14:14:10 +0530

    session history changed

--- src/pages/app/admin/homepage/index.jsx
+++ src/pages/app/admin/homepage/index.jsx
@@ -1,75 +1,74 @@
 import React from "react";
 import styles from "./homepage.module.css";
 import Button from "../../../../components/common/button";
-import svg from "../../../../assets/images/session-history-bg.svg"
+import PageContainer from "../../../../components/ui/pagecontainer";
 
 const Homepage = () => {
   return (
-    <div>
-      <h2>
-        Welcome Admin
-      </h2>
-
-      {/* Session History:: start */}
-      <div className={styles.sessionHistoryContainer}>
-        <h3>Session History</h3>
-        <div className={styles.sessionHistoryCardContainer}>
-          <div className={styles.sessionHistoryCard}>
-            <h4>Pwc Risk Session</h4>
-            <p>Scenario:</p>
-            <p>Status:</p>
-            <div className={styles.butonFlexContainer}>
-              <div className={styles.updatedDate}>
-                <p>Updated: </p>
-              </div>
-              <div>
-                <Button customClassName={styles.button}>
-                  Start
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className={styles.sessionHistoryCard}>
-            <h4>Pwc Risk Session</h4>
-            <p>Scenario:</p>
-            <p>Status:</p>
-            <div className={styles.butonFlexContainer}>
-              <div className={styles.updatedDate}>
-                <p>Updated: </p>
-              </div>
-              <div>
-                <Button customClassName={styles.button}>
-                  Start
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className={styles.sessionHistoryCard}>
-            <h4>Pwc Risk Session</h4>
-            <p>Scenario:</p>
-            <p>Status:</p>
-            <div className={styles.butonFlexContainer}>
-              <div className={styles.updatedDate}>
-                <p>Updated: </p>
-              </div>
-              <div>
-                <Button customClassName={styles.button}>
-                  Start
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      {/* Session History:: end */}
-
-      {/* Scenarios:: start */}
+    <PageContainer>
       <div>
-        Scenarios
+        <h2>
+          Welcome Admin
+        </h2>
+
+        {/* Session History:: start */}
+        <div className={styles.sessionHistoryContainer}>
+          <h3>Session History</h3>
+          <div className={styles.sessionHistoryCardContainer}>
+            <div className={styles.sessionHistoryCard}>
+              <h4>Pwc Risk Session</h4>
+              <p>Scenario:</p>
+              <p>Status:</p>
+              <div className={styles.butonFlexContainer}>
+                <div className={styles.updatedDate}>
+                  <p>Updated: </p>
+                </div>
+                <div>
+                  <Button customClassName={styles.button}>
+                    Start
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className={styles.sessionHistoryCard}>
+              <h4>Pwc Risk Session</h4>
+              <p>Scenario:</p>
+              <p>Status:</p>
+              <div className={styles.butonFlexContainer}>
+                <div className={styles.updatedDate}>
+                  <p>Updated: </p>
+                </div>
+                <div>
+                  <Button customClassName={styles.button}>
+                    Start
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className={styles.sessionHistoryCard}>
+              <h4>Pwc Risk Session</h4>
+              <p>Scenario:</p>
+              <p>Status:</p>
+              <div className={styles.butonFlexContainer}>
+                <div className={styles.updatedDate}>
+                  <p>Updated: </p>
+                </div>
+                <div>
+                  <Button customClassName={styles.button}>
+                    Start
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div>
+          Scenarios
+        </div>
       </div>
-      {/* Scenarios:: end */}
-    </div>
+    </PageContainer>
+
   );
 };
 
