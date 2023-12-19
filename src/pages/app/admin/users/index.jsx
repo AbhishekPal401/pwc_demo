1ee2ef00ed6e9a27a9baff9a8cfb4ec77ca9b0e0
abhishek.kp <abhishek.kp@dkv.global>
2023-12-19 13:36:18 +0530

    changes

---
+++ src/pages/app/admin/users/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageContainer from "../../../../components/ui/pagecontainer";
+
+const Users = () => {
+  return (
+    <PageContainer>
+      <div>1</div>
+    </PageContainer>
+  );
+};
+
+export default Users;
