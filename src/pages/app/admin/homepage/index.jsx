ce279b91fd05d8c99d1d11b2494bef30357c0a83
Rushibhawar <rushikeshbhawar1213@gmail.com>
2023-12-19 16:49:24 +0530

    session-history changes

--- src/pages/app/admin/homepage/index.jsx
+++ src/pages/app/admin/homepage/index.jsx
@@ -11,14 +11,14 @@ const Homepage = () => {
 
   return (
     <PageContainer>
-        <div className={styles.topContainer}>
-          <div className={styles.left}>
-            <label>Welcome Admin</label>
-          </div>
-          <div className={styles.right}>
-            <img src="./images/home.png" alt="home background png" />
-          </div>
+      <div className={styles.topContainer}>
+        <div className={styles.left}>
+          <label>Welcome Admin</label>
         </div>
+        <div className={styles.right}>
+          <img src="./images/home.png" alt="home background png" />
+        </div>
+      </div>
       <div className={styles.mainContainer}>
         {/* Session History:: start */}
         <div className={styles.sessionHistoryContainer}>
@@ -45,90 +45,102 @@ const Homepage = () => {
         {/* Session History:: end */}
 
         {/* Scenario Table:: start */}
-        <div className={styles.mainTableContainer}>
-          <table className={styles.table_content}>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Username</th>
-                <th>EmailId</th>
-                <th>Designation</th>
-                <th>Organistion</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
+        <div>
+          <div className={styles.scenarioTopContainer}>
+            <div className={styles.scenarioTopContainerRight}>
+              <div><h3>Scenarios</h3></div>
+              <div><a href="#">See All</a></div>
+            </div>
+            <div className={styles.scenarioTopContainerLeft}>
+              <Button>Create New</Button>
+            </div>
+          </div>
+          <div className={styles.mainTableContainer}>
+            <table className={styles.table_content}>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Username</th>
+                  <th>EmailId</th>
+                  <th>Designation</th>
+                  <th>Organistion</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
 
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>User</td>
-                <td>Email@gmail.com</td>
-                <td>CFO</td>
-                <td>PWC</td>
-                <td>button</td>
-              </tr>
-            </tbody>
-          </table>
-          <div className={styles.paginationContainer}>
-            <Pagination totalCount={100} pageNumber={10} countPerPage={10} />
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>User</td>
+                  <td>Email@gmail.com</td>
+                  <td>CFO</td>
+                  <td>PWC</td>
+                  <td>button</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className={styles.paginationContainer}>
+              <Pagination totalCount={100} pageNumber={10} countPerPage={10} />
+            </div>
           </div>
         </div>
+
         {/* Scenario Table:: end */}
       </div>
 
